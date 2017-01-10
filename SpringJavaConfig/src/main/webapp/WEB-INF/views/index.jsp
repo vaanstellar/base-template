06d463fbd73da3dc86db031658f02ba90b1fe51f
Ivan.Kurniawan <vaanstellar20@gmail.com>
2017-01-10 10:30:00 +0800

    Initial commit to GIT

---
+++ SpringJavaConfig/src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>${helloMessage} }</h2>
+</body>
+</html>
---
+++ SpringJavaConfig/src/test/java/com/van/base/controller/AppController.java
@@ -0,0 +1,27 @@
+package com.van.base.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class AppController {
+
+	@Autowired
+	MessageSource messageSource;
+
+	/**
+	 * This method will list all existing users.
+	 */
+	@RequestMapping(value = { "/" }, method = RequestMethod.GET)
+	public String listUsers(ModelMap model) {
+
+		model.addAttribute("helloMessage", "Spring with java configuration base template");
+		return "index";
+	}
+
+}
